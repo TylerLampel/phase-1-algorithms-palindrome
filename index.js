727dd0c5955d29714084ce84d3de6e641a6f7a1d
Tyler Lampel <lampel13@gmail.com>
2022-05-28 22:29:22 -0400

    commit tree 2d83854d9ee6dfccf8131a880245a9c83f1fe8cd

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = word.split('').reverse().join('');
+      if (reverseWord === word) {
+        return true
+      } else {
+        return false
+      }
 }
 
 /* 
   Add your pseudocode here
+  define isPallindrome function
+  set variable for reverse string
+  compare if they are ===
+  return true or false
 */
 
 /*
   Add written explanation of your solution here
+  create a const of the reverse string
+  split the string into an array of strings
+  reverse the string
+  join strings in reverse into a string
+  strictly compare and return true or false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("true", isPalindrome("aa"))
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
